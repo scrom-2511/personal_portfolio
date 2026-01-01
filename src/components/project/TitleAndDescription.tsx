daff71b78008449bc7e1b8d6b5892da61100b2c3
scrom <formain337@gmail.com>
2026-01-01 20:10:22 +0000

    add: title and description component for project page

---
+++ src/components/project/TitleAndDescription.tsx
@@ -0,0 +1,17 @@
+import { CardDescription, CardTitle } from "../ui/card";
+
+const TitleAndDescription = () => {
+  return (
+    <div>
+      <CardTitle>Project Name</CardTitle>
+      <CardDescription>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae rerum ex
+        debitis architecto commodi inventore, minus doloremque officia
+        repellendus aut perferendis quidem nam optio blanditiis eveniet eum
+        voluptas odit reprehenderit veritatis obcaecati!
+      </CardDescription>
+    </div>
+  );
+};
+
+export default TitleAndDescription;
