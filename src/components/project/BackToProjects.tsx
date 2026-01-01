ca319680846510225edf51b1f891c50d586e1eae
scrom <formain337@gmail.com>
2026-01-01 19:50:56 +0000

    add: go back button in project page

---
+++ src/components/project/BackToProjects.tsx
@@ -0,0 +1,10 @@
+import { CircleChevronLeft } from "lucide-react";
+import { Button } from "../ui/button";
+
+export const BackToProjects = () => {
+  return (
+    <Button variant={"outline"}>
+      <CircleChevronLeft></CircleChevronLeft> Go back
+    </Button>
+  );
+};
