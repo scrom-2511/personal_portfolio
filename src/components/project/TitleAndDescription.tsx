329a3a9bbacb119fa1655efc6129f019cf30aa62
scrom <formain337@gmail.com>
2026-01-01 20:14:06 +0000

    modify: added some bottom margin to the proect name

--- src/components/project/TitleAndDescription.tsx
+++ src/components/project/TitleAndDescription.tsx
@@ -3,8 +3,8 @@ import { CardDescription, CardTitle } from "../ui/card";
 const TitleAndDescription = () => {
   return (
     <div>
-      <CardTitle>Project Name</CardTitle>
-      <CardDescription>
+      <CardTitle className="mb-2 text-3xl">Project Name</CardTitle>
+      <CardDescription >
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae rerum ex
         debitis architecto commodi inventore, minus doloremque officia
         repellendus aut perferendis quidem nam optio blanditiis eveniet eum
