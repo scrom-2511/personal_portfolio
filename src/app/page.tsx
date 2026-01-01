81eef517fe2496de4cccb83890f68e586358ee23
scrom <formain337@gmail.com>
2026-01-01 19:10:29 +0000

    add: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,13 @@ function CardFooterMain() {
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-[#1c191a]">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-5 py-32 bg-white dark:bg-[#1c191a] sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-5 py-10 bg-white dark:bg-[#1c191a] sm:items-start">
+        <div className="flex flex-row gap-5">
+          <a href=""><Github></Github></a>
+          <a href=""><Linkedin></Linkedin></a>
+          <a href=""><Twitter></Twitter></a>
+          <a href=""><Mail></Mail></a>
+        </div>
         <ProfileCard />
         <ProjectsCard/>
       </main>
