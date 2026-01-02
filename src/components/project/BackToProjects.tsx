d49a799064df1f41d949efe3a74f755bfab20ad0
scrom <formain337@gmail.com>
2026-01-02 03:58:23 +0000

    add: page routing from project page to home

--- src/components/project/BackToProjects.tsx
+++ src/components/project/BackToProjects.tsx
@@ -1,9 +1,15 @@
+"use client";
 import { CircleChevronLeft } from "lucide-react";
 import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 export const BackToProjects = () => {
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push("/");
+  };
   return (
-    <Button variant={"outline"}>
+    <Button variant={"outline"} onClick={handleOnClick}>
       <CircleChevronLeft></CircleChevronLeft> Go back
     </Button>
   );
